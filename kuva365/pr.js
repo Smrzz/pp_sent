891a750af32ec93de1b94f204caad0571d75a0f0
Smertz <ivan.kishkin2004@yandex.com>
2024-09-27 08:13:05 +0500

    ready?

--- kuva365/pr.js
+++ kuva365/pr.js
@@ -2,16 +2,16 @@ function openForm(formId) {
     const form = document.getElementById(formId);
     form.style.display = "block";
     
-    // Устанавливаем форму по центру экрана
+   
     form.style.position = "fixed";
     form.style.top = "50%";
     form.style.left = "50%";
     form.style.transform = "translate(-50%, -50%)";
     
-    // Добавляем обработчик клика для закрытия формы при клике вне её области
+
     setTimeout(() => {
         document.addEventListener('click', closeFormOnClickOutside);
-    }, 0); // Используем setTimeout, чтобы избежать закрытия сразу после открытия
+    }, 0); 
 }
 
 function closeForm(formId) {
@@ -25,7 +25,7 @@ function closeFormOnClickOutside(e) {
     
     forms.forEach((form) => {
         if (form.style.display === "block" && !form.contains(e.target) && !e.target.closest('.btn-gast')) {
-            form.style.display = "none"; // Закрываем все формы, если клик вне их области
+            form.style.display = "none"; 
         }
     });
 }
